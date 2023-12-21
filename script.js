2766ef87b18aee111f061761d84ddf8d671d5374
Jovin Shija <jovination791@gmail.com>
2023-12-21 16:02:13 +0300

    integrating plans buttons for both monltly

--- script.js
+++ script.js
@@ -9,13 +9,40 @@ document.addEventListener('DOMContentLoaded', function () {
   monthlyOp.onclick = () => {
       monthlyOp.classList.add('active');
       yearlyOp.classList.remove('active');
+      showMonthlyPlans();
+
   };
 
   yearlyOp.onclick = () => {
       monthlyOp.classList.remove('active');
       yearlyOp.classList.add('active');
+      showYearlyPlans();
+
   };
+  showMonthlyPlans();
 });
+
+function showMonthlyPlans() {
+  document.querySelectorAll('.monthly-plan').forEach(plan => {
+      plan.style.display = 'block';
+  });
+  document.querySelectorAll('.yearly-plan').forEach(plan => {
+    plan.style.display = 'none';
+});
+}
+
+function showYearlyPlans() {
+  document.querySelectorAll('.monthly-plan').forEach(plan => {
+      plan.style.display = 'none';
+  });
+
+  document.querySelectorAll('.yearly-plan').forEach(plan => {
+      plan.style.display = 'block';
+  });
+}
+
+
+
 
 //container package when clicked
 function priceContainer(price__container){
