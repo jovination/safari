03d0a08ea38527130cd0de74f3f2f393b3af77f1
jovination <jovination791@gmail.com>
2024-03-12 21:34:54 +0300

    few updates

--- script.js
+++ script.js
@@ -61,21 +61,26 @@ function priceContainer(price__container){
   price__container.classList.add('clicked');
 }
 
-//FAQS accordion
 const faq__header = document.querySelectorAll('.faq__header');
 
-faq__header.forEach ((faq__header) => {
-  faq__header.addEventListener('click', () => {
+faq__header.forEach((header) => {
+  header.addEventListener('click', () => {
+    // Toggle the 'is__open' class
+    header.classList.toggle('is__open');
 
-    if(faq__header.classList.contains('is__open')){
-      faq__header.classList.remove('is__open');
-    }
-    else{
-      const faq__header__is__open = document.querySelectorAll('.is__open');
-      faq__header__is__open.forEach((faq__header__is__open) => {
-        faq__header__is__open.classList.remove('is__open');
-      });
-      faq__header.classList.add('is__open');
-    }
-  })
-})
+    // Toggle the plus and minus icons
+    const icon = header.querySelector("i");
+    icon.classList.toggle("fa-plus");
+    icon.classList.toggle("fa-minus");
+
+    // Close other open headers if any
+    const openHeaders = document.querySelectorAll('.faq__header.is__open');
+    openHeaders.forEach((openHeader) => {
+      if (openHeader !== header) {
+        openHeader.classList.remove('is__open');
+        // Change the icon to plus for other open headers
+        openHeader.querySelector("i").classList.replace("fa-minus", "fa-plus");
+      }
+    });
+  });
+});
